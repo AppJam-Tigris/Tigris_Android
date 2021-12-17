ac84569ec9f572f6b8925514457a51cd77cc1b13
limsaehyun <sh00710000@gmail.com>
2021-12-17 20:17:08 +0900

    Feat :: RegisterApi

---
+++ app/src/main/java/com/saehyun/trigris_android/data/register/RegisterApi.kt
@@ -0,0 +1,17 @@
+package com.saehyun.trigris_android.data.register
+
+import com.saehyun.trigris_android.feature.register.model.RegisterRequest
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.PUT
+
+interface RegisterApi {
+
+    // register
+    @POST("student")
+    fun register(
+        @Body registerRequest: RegisterRequest
+    ) : Single<Response<Void>>
+}
