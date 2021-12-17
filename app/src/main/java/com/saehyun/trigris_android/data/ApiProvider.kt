e595e39d6446d5da382e7a7edc1a54687770faba
limsaehyun <sh00710000@gmail.com>
2021-12-17 20:15:46 +0900

    Feat :: ApiProvider

---
+++ app/src/main/java/com/saehyun/trigris_android/data/ApiProvider.kt
@@ -0,0 +1,28 @@
+package com.example.nms_android_v1.di
+
+import BASE_URL
+import com.saehyun.trigris_android.data.login.LoginApi
+import com.saehyun.trigris_android.data.main.MainApi
+import com.saehyun.trigris_android.data.register.RegisterApi
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+private val retrofit: Retrofit = Retrofit.Builder().apply {
+    baseUrl(BASE_URL)
+    addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+    addConverterFactory(GsonConverterFactory.create())
+}.build()
+
+val loginApi : LoginApi by lazy {
+    retrofit.create(LoginApi::class.java)
+}
+
+val registerApi : RegisterApi by lazy {
+    retrofit.create(RegisterApi::class.java)
+}
+
+val mainApi : MainApi by lazy {
+    retrofit.create(MainApi::class.java)
+}
