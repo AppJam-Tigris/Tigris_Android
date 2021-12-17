b3059c2ed0924cbacf007cc793aec4c0bbcc57be
limsaehyun <sh00710000@gmail.com>
2021-12-17 20:17:14 +0900

    Feat :: RegisterRepository

---
+++ app/src/main/java/com/saehyun/trigris_android/data/register/RegisterRepository.kt
@@ -0,0 +1,17 @@
+package com.saehyun.trigris_android.data.register
+
+import com.example.nms_android_v1.di.registerApi
+import com.saehyun.trigris_android.feature.register.model.RegisterRequest
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
+class RegisterRepository {
+
+    fun register(registerRequest: RegisterRequest) : @NonNull Single<Response<Void>> =
+        registerApi.register(registerRequest)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+}
