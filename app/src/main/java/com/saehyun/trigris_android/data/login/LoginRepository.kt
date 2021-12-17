aaa3b21a4cc81eced04ac4f38371195469e4703d
limsaehyun <sh00710000@gmail.com>
2021-12-17 20:16:54 +0900

    Feat :: LoginRepository

---
+++ app/src/main/java/com/saehyun/trigris_android/data/login/LoginRepository.kt
@@ -0,0 +1,19 @@
+package com.saehyun.trigris_android.data.login
+
+import com.example.nms_android_v1.di.loginApi
+import com.saehyun.trigris_android.feature.login.model.LoginRequest
+import com.saehyun.trigris_android.feature.login.model.LoginResponse
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
+class LoginRepository {
+
+    fun login(loginRequest: LoginRequest) : @NonNull Single<Response<LoginResponse>> =
+        loginApi.login(loginRequest)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+
+}
