f7c098199dc001613957e7dc4859b5b837a8dbd2
limsaehyun <sh00710000@gmail.com>
2021-12-17 20:20:06 +0900

    Feat :: base상속

---
+++ app/src/main/java/com/saehyun/trigris_android/feature/register/ui/RegisterActivity.kt
@@ -0,0 +1,17 @@
+package com.saehyun.trigris_android.feature.register.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nms_android_v1.base.BaseActivity
+import com.saehyun.trigris_android.R
+import com.saehyun.trigris_android.databinding.ActivityRegisterBinding
+
+class RegisterActivity : BaseActivity<ActivityRegisterBinding> (
+    R.layout.activity_register
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun observeEvent() {}
+}
