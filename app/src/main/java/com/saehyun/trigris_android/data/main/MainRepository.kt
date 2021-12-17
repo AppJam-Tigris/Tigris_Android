728fa0278b167f1f1a03adbf796b0ed7489c183d
limsaehyun <sh00710000@gmail.com>
2021-12-17 20:17:04 +0900

    Feat :: MainRepository

---
+++ app/src/main/java/com/saehyun/trigris_android/data/main/MainRepository.kt
@@ -0,0 +1,17 @@
+package com.saehyun.trigris_android.data.main
+
+import ACCESS_TOKEN
+import com.example.nms_android_v1.di.mainApi
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
+import retrofit2.Response
+
+class MainRepository {
+
+//    fun getMain() : @NonNull Single<Response<PostsResponse>> =
+//        mainApi.getPosts(ACCESS_TOKEN)
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribeOn(Schedulers.io())
+}
