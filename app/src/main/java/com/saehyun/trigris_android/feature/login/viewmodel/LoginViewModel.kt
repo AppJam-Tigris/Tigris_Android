7f82981f734c3474829ae52de7847724c9f801fd
limsaehyun <sh00710000@gmail.com>
2021-12-17 20:18:40 +0900

    Feat :: LoginViewModel

---
+++ app/src/main/java/com/saehyun/trigris_android/feature/login/viewmodel/LoginViewModel.kt
@@ -0,0 +1,29 @@
+package com.saehyun.trigris_android.feature.login.viewmodel
+
+import ACCESS_TOKEN
+import REFRESH_TOKEN
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.saehyun.trigris_android.data.login.LoginRepository
+import com.saehyun.trigris_android.feature.login.model.LoginRequest
+import org.koin.core.Koin
+
+class LoginViewModel(
+    private val rp: LoginRepository
+) : ViewModel() {
+
+    val success : MutableLiveData<Boolean> = MutableLiveData()
+    val failed : MutableLiveData<Boolean> = MutableLiveData()
+
+    fun login(loginRequest: LoginRequest) {
+        rp.login(loginRequest).subscribe { response ->
+            if(response.isSuccessful) {
+                ACCESS_TOKEN = "Bearer " + response.body()?.accessToken.toString()
+                REFRESH_TOKEN = response.body()?.refreshToken.toString()
+                success.value = true
+            } else {
+                failed.value = true
+            }
+        }
+    }
+}
