7b7bec743975ed9bdb531be839d41e633b2fb6f2
limsaehyun <sh00710000@gmail.com>
2021-12-17 20:16:59 +0900

    Feat :: MainApi

---
+++ app/src/main/java/com/saehyun/trigris_android/data/main/MainApi.kt
@@ -0,0 +1,14 @@
+package com.saehyun.trigris_android.data.main
+
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface MainApi {
+//    @GET("notice/all")
+//    fun getMain(
+//        @Header("Authorization") accessToken: String
+//    ) : Single<Response<PostsResponse>>
+}
