e73b17434bc37aa2fd74995059fa3cca79574153
limsaehyun <sh00710000@gmail.com>
2021-12-17 20:16:38 +0900

    Feat :: LoginApi

---
+++ app/src/main/java/com/saehyun/trigris_android/data/login/LoginApi.kt
@@ -0,0 +1,16 @@
+package com.saehyun.trigris_android.data.login
+
+import com.saehyun.trigris_android.feature.login.model.LoginRequest
+import com.saehyun.trigris_android.feature.login.model.LoginResponse
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface LoginApi {
+
+    @POST("student/auth")
+    fun login(
+        @Body loginRequest: LoginRequest
+    ) : Single<Response<LoginResponse>>
+}
