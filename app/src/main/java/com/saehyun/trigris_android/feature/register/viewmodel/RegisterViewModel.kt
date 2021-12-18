daa94688b70daeb0b4322e95417a3aba18b23ab1
limsaehyun <sh00710000@gmail.com>
2021-12-19 05:18:02 +0900

    Feat :: idCheck & phoneCheck viewModel

--- app/src/main/java/com/saehyun/trigris_android/feature/register/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/saehyun/trigris_android/feature/register/viewmodel/RegisterViewModel.kt
@@ -3,29 +3,61 @@ package com.saehyun.trigris_android.feature.register.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.saehyun.trigris_android.data.register.RegisterRepository
+import com.saehyun.trigris_android.feature.register.model.IdCheckRequest
 import com.saehyun.trigris_android.feature.register.model.Location
+import com.saehyun.trigris_android.feature.register.model.PhoneCheckRequest
 import com.saehyun.trigris_android.feature.register.model.RegisterRequest
 
 class RegisterViewModel(
     private val rp: RegisterRepository
 ) : ViewModel() {
 
-    val success : MutableLiveData<Boolean> = MutableLiveData()
-    val failed : MutableLiveData<String> = MutableLiveData()
-    val errorMessage : MutableLiveData<String> = MutableLiveData()
-    val checkSuccess : MutableLiveData<Boolean> = MutableLiveData()
-    val checkFailed : MutableLiveData<Boolean> = MutableLiveData()
+    val registerSuccess : MutableLiveData<Boolean> = MutableLiveData()
+    val registerFailed : MutableLiveData<String> = MutableLiveData()
 
-    fun register(name: String, phoneNumber: String, birthDay: String, gender: String, nationality: String,
-                 location: Location, uid: String, password: String) {
-        val registerRequest = RegisterRequest(name, phoneNumber, birthDay, gender, nationality, location, uid, password)
+    val idCheckSuccess : MutableLiveData<Boolean> = MutableLiveData()
+    val idCheckFailed : MutableLiveData<String> = MutableLiveData()
+
+    val phoneCheckSuccess : MutableLiveData<Boolean> = MutableLiveData()
+    val phoneCheckFailed : MutableLiveData<String> = MutableLiveData()
+
+    fun register(registerRequest: RegisterRequest) {
         rp.register(registerRequest).subscribe { response ->
             if(response.isSuccessful) {
-                success.value = true
+                registerSuccess.value = true
             } else {
                 when(response.code()) {
-                    409 -> failed.value = "이미 존재하는 계정입니다."
-                    else -> failed.value = "회원가입에 실패했습니다."
+                    400 -> registerFailed.value = "회원가입 데이터가 올바르지 않습니다."
+                    401 -> registerFailed.value = "인증번호가 올바르지 않습니다."
+                    409 -> registerFailed.value = "닉네임 또는 이메일 또는 (학번, 이름)가 이미 존재합니다."
+                    else -> idCheckFailed.value = "데이터 통신에 실패했습니다.\n다시 시도해주세요."
+                }
+            }
+        }
+    }
+
+    fun idCheck(uid: String) {
+        val idCheckRequest = IdCheckRequest(uid)
+        rp.idCheck(idCheckRequest).subscribe { response ->
+            if(response.isSuccessful) {
+                idCheckSuccess.value = true
+            } else {
+                when(response.code()) {
+                    409 -> idCheckFailed.value = "이미 존재하는 계정입니다."
+                    else -> idCheckFailed.value = "데이터 통신에 실패했습니다.\n다시 시도해주세요."
+                }
+            }
+        }
+    }
+
+    fun phoneCheck(phoneNumber: String) {
+        val phoneCheckRequest = PhoneCheckRequest(phoneNumber)
+        rp.phoneCheck(phoneCheckRequest).subscribe { response ->
+            if(response.isSuccessful) {
+                phoneCheckSuccess.value = true
+            } else {
+                when(response.code()) {
+                    else -> phoneCheckFailed.value = "데이터 통신에 실패했습니다.\n다시 시도해주세요."
                 }
             }
         }
