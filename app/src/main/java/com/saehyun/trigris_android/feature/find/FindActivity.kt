67d39e6254bf2c768063637fc43c016f99364108
limsaehyun <sh00710000@gmail.com>
2021-12-19 05:38:43 +0900

    Feat :: FindActivity

---
+++ app/src/main/java/com/saehyun/trigris_android/feature/find/FindActivity.kt
@@ -0,0 +1,18 @@
+package com.saehyun.trigris_android.feature.find
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nms_android_v1.base.BaseActivity
+import com.saehyun.trigris_android.R
+import com.saehyun.trigris_android.databinding.ActivityFindBinding
+
+class FindActivity : BaseActivity<ActivityFindBinding> (
+    R.layout.activity_find
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun observeEvent() {}
+}
