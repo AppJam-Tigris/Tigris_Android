156178c9671ced516c87ac02d7a4e5f766598259
limsaehyun <sh00710000@gmail.com>
2021-12-18 16:54:38 +0900

    Feat :: 로그인 기능 구현

--- app/src/main/java/com/saehyun/trigris_android/feature/login/ui/LoginActivity.kt
+++ app/src/main/java/com/saehyun/trigris_android/feature/login/ui/LoginActivity.kt
@@ -1,10 +1,12 @@
 package com.saehyun.trigris_android.feature.login.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nms_android_v1.base.BaseActivity
 import com.saehyun.trigris_android.R
 import com.saehyun.trigris_android.databinding.ActivityLoginBinding
+import com.saehyun.trigris_android.feature.main.ui.MainActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding> (
     R.layout.activity_login
@@ -12,8 +14,28 @@ class LoginActivity : BaseActivity<ActivityLoginBinding> (
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        binding.tvLogin.setOnClickListener {
+            login()
+        }
+
     }
 
     override fun observeEvent() {
+    }
+
+    private fun login() {
+        val id = binding.etLoginId.text.toString()
+        val pw = binding.etLoginPw.text.toString()
+
+        if(id.isNotEmpty() && pw.isNotEmpty()) {
+            startMain()
+        } else {
+            showToast("아이디 또는 비밀번호를 입력해주세요.")
+        }
+    }
+
+    private fun startMain() {
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
