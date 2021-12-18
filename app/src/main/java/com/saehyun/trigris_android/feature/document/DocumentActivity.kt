aa0991d6119aaba24eccee780161c906cd3ccf02
limsaehyun <sh00710000@gmail.com>
2021-12-19 07:25:13 +0900

    Feat :: DocumentActivity

---
+++ app/src/main/java/com/saehyun/trigris_android/feature/document/DocumentActivity.kt
@@ -0,0 +1,12 @@
+package com.saehyun.trigris_android.feature.document
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saehyun.trigris_android.R
+
+class DocumentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_document)
+    }
+}
