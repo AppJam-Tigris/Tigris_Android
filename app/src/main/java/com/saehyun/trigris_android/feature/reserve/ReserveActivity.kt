d7fb0ca80928ef1d1def7e8ea3fbd4907c318382
limsaehyun <sh00710000@gmail.com>
2021-12-19 05:52:30 +0900

    Feat :: create Reserve

---
+++ app/src/main/java/com/saehyun/trigris_android/feature/reserve/ReserveActivity.kt
@@ -0,0 +1,17 @@
+package com.saehyun.trigris_android.feature.reserve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nms_android_v1.base.BaseActivity
+import com.saehyun.trigris_android.R
+import com.saehyun.trigris_android.databinding.ActivityReserveBinding
+
+class ReserveActivity : BaseActivity<ActivityReserveBinding> (
+    R.layout.activity_reserve
+) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun observeEvent() {}
+}
