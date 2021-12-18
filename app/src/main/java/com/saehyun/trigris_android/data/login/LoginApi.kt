fdb2a5a5cdced02669ad1cb446be6bbf8217ee22
limsaehyun <sh00710000@gmail.com>
2021-12-19 01:25:00 +0900

    Feat :: 회원정보 불러오기

--- app/src/main/java/com/saehyun/trigris_android/data/login/LoginApi.kt
+++ app/src/main/java/com/saehyun/trigris_android/data/login/LoginApi.kt
@@ -5,12 +5,21 @@ import com.saehyun.trigris_android.feature.login.model.LoginResponse
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface LoginApi {
 
+    // 로그인
     @POST("user/auth")
     fun login(
         @Body loginRequest: LoginRequest
+    ) : Single<Response<LoginResponse>>
+
+    // 회원정보 불러오기
+    @GET("user")
+    fun getInfo(
+        @Header("Authorization") accessToken: String
     ) : Single<Response<LoginResponse>>
 }
