c9c120b6919f6e438979240c74b02f432e04dd19
limsaehyun <sh00710000@gmail.com>
2021-12-19 08:43:48 +0900

    Feat :: DocumentAcitivty

--- app/src/main/java/com/saehyun/trigris_android/feature/document/DocumentActivity.kt
+++ app/src/main/java/com/saehyun/trigris_android/feature/document/DocumentActivity.kt
@@ -1,12 +1,31 @@
 package com.saehyun.trigris_android.feature.document
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.saehyun.trigris_android.R
 
 class DocumentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_document)
+
+        findViewById<TextView>(R.id.sstvFind).setOnClickListener {
+            startActivity(Intent(applicationContext, FinishActivity::class.java))
+        }
+
+        findViewById<TextView>(R.id.textView15).setOnClickListener {
+            findViewById<TextView>(R.id.textView15).background = ContextCompat.getDrawable(this, R.drawable.bg_finish_stoke_border)
+        }
+
+        findViewById<TextView>(R.id.textView14).setOnClickListener {
+            findViewById<TextView>(R.id.textView14).background = ContextCompat.getDrawable(this, R.drawable.bg_finish_stoke_border)
+        }
+
+        findViewById<TextView>(R.id.textView12).setOnClickListener {
+            findViewById<TextView>(R.id.textView12).background = ContextCompat.getDrawable(this, R.drawable.bg_finish_stoke_border)
+        }
     }
 }
