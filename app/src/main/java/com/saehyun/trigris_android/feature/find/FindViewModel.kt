7a2fb35ae5b9729213d460759bbd83631292d17b
limsaehyun <sh00710000@gmail.com>
2021-12-19 08:44:19 +0900

    Feat :: FindViewModel

---
+++ app/src/main/java/com/saehyun/trigris_android/feature/find/FindViewModel.kt
@@ -0,0 +1,29 @@
+package com.saehyun.trigris_android.feature.find
+
+import ACCESS_TOKEN
+import REFRESH_TOKEN
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.saehyun.trigris_android.data.find.FindRepository
+import com.saehyun.trigris_android.data.login.LoginRepository
+import com.saehyun.trigris_android.feature.find.model.ClinicRequest
+import com.saehyun.trigris_android.feature.find.model.ClinicResponse
+import com.saehyun.trigris_android.feature.login.model.LoginRequest
+
+class FindViewModel(
+    private val fp: FindRepository
+) : ViewModel() {
+
+    val clinicData : MutableLiveData<ClinicResponse> = MutableLiveData()
+    val failed : MutableLiveData<Boolean> = MutableLiveData()
+
+    fun getClinic() {
+        fp.getClinic().subscribe { response ->
+            if(response.isSuccessful) {
+                clinicData.value = response.body()
+            } else {
+                failed.value = true
+            }
+        }
+    }
+}
