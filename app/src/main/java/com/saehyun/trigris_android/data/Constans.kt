5df3851d470e194802cfe9985a7d46502ecd39b1
limsaehyun <sh00710000@gmail.com>
2021-12-18 20:03:38 +0900

    Feat :: MAP URL 저장

--- app/src/main/java/com/saehyun/trigris_android/data/Constans.kt
+++ app/src/main/java/com/saehyun/trigris_android/data/Constans.kt
@@ -1,5 +1,7 @@
 
 val BASE_URL = "URL"
 
+val SEOUL_MAP_URL = "https://www.google.com/url?sa=t&source=web&rct=j&url=https://map.seoul.go.kr/smgis2/smap/XkJBU0VNQVBfR0VOXlQ6MTFeRl5GXkZeXl5eXl4xMjYuOTc4NTc2XjM3LjU2NjUwMl42XjExMTAzMzUwXmNpdHlMaWZlXl5eLg%3D%3D&ved=2ahUKEwiIqZ_tiu30AhUMsVYBHayXBX0QFnoECAUQAQ&usg=AOvVaw1867bIfLGrep2REgdFRojZ"
+
 var ACCESS_TOKEN: String = "default"
 var REFRESH_TOKEN: String = "default"
