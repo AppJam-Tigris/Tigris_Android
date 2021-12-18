9808ca3e01c464df838c5e2c7de9ab9339ad5dc6
limsaehyun <sh00710000@gmail.com>
2021-12-18 20:03:51 +0900

    Feat :: Koin Container

--- app/src/main/java/com/saehyun/trigris_android/di/Container.kt
+++ app/src/main/java/com/saehyun/trigris_android/di/Container.kt
@@ -1,4 +1,24 @@
 package com.saehyun.trigris_android.di
 
-class Container {
+import android.app.Application
+import com.saehyun.trigris_android.di.module.loginModule
+import com.saehyun.trigris_android.di.module.registerModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class Container: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@Container)
+
+            modules(
+                listOf(
+                    loginModule,
+                    registerModule
+                )
+            )
+        }
+    }
 }
