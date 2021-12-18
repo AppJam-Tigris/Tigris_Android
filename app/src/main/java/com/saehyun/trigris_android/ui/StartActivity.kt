661233fe998a61ad2c25d3afa6a0d29ee7b030f3
limsaehyun <sh00710000@gmail.com>
2021-12-18 16:54:55 +0900

    Feat :: startLogin

--- app/src/main/java/com/saehyun/trigris_android/ui/StartActivity.kt
+++ app/src/main/java/com/saehyun/trigris_android/ui/StartActivity.kt
@@ -1,10 +1,12 @@
 package com.saehyun.trigris_android.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nms_android_v1.base.BaseActivity
 import com.saehyun.trigris_android.R
 import com.saehyun.trigris_android.databinding.ActivityStartBinding
+import com.saehyun.trigris_android.feature.login.ui.LoginActivity
 
 class StartActivity : BaseActivity<ActivityStartBinding> (
     R.layout.activity_start
@@ -12,8 +14,13 @@ class StartActivity : BaseActivity<ActivityStartBinding> (
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        startLogin()
     }
 
-    override fun observeEvent() {
+    override fun observeEvent() {}
+
+    private fun startLogin() {
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
