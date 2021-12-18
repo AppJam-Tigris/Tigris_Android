a29a12ee82bd128f0aaead2da5d8f6f36f7a060a
limsaehyun <sh00710000@gmail.com>
2021-12-19 01:25:12 +0900

    Feat :: 전화번호 인증번호 발송 & 아이디 중복 확인

--- app/src/main/java/com/saehyun/trigris_android/data/register/RegisterApi.kt
+++ app/src/main/java/com/saehyun/trigris_android/data/register/RegisterApi.kt
@@ -1,5 +1,7 @@
 package com.saehyun.trigris_android.data.register
 
+import com.saehyun.trigris_android.feature.register.model.IdCheckRequest
+import com.saehyun.trigris_android.feature.register.model.PhoneCheckRequest
 import com.saehyun.trigris_android.feature.register.model.RegisterRequest
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
@@ -8,9 +10,21 @@ import retrofit2.http.POST
 
 interface RegisterApi {
 
-    // register
+    // 회원가입
     @POST("user")
     fun register(
         @Body registerRequest: RegisterRequest
+    ) : Single<Response<Void>>
+
+    // 전화번호 인증번호 발송
+    @POST("user/home")
+    fun phoneCheck(
+        @Body phoneCheckRequest: PhoneCheckRequest
+    ) : Single<Response<Void>>
+
+    // 아이디 중복 확인
+    @POST("user/duplicate")
+    fun idCheck(
+        @Body idCheckRequest: IdCheckRequest
     ) : Single<Response<Void>>
 }
