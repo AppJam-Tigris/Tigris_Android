1e3ff4e2ae6218ece674df18cf2d8c4605c7525b
limsaehyun <sh00710000@gmail.com>
2021-12-18 20:04:30 +0900

    Feat :: 회원가입에 필요한 변수

--- app/src/main/java/com/saehyun/trigris_android/feature/register/ui/RegisterActivity.kt
+++ app/src/main/java/com/saehyun/trigris_android/feature/register/ui/RegisterActivity.kt
@@ -5,13 +5,33 @@ import android.os.Bundle
 import com.example.nms_android_v1.base.BaseActivity
 import com.saehyun.trigris_android.R
 import com.saehyun.trigris_android.databinding.ActivityRegisterBinding
+import com.saehyun.trigris_android.feature.register.model.Location
+import com.saehyun.trigris_android.feature.register.model.RegisterRequest
+import com.saehyun.trigris_android.feature.register.viewmodel.RegisterViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RegisterActivity : BaseActivity<ActivityRegisterBinding> (
     R.layout.activity_register
 ) {
+
+    private val vm: RegisterViewModel by viewModel()
+
+    private var name: String ?= null
+    private var phone_number: String ?= null
+    private var birth_day: String ?= null
+    private var gender: String ?= null
+    private var nationality: String ?= null
+    private var location: Location ?= null
+    private var uid: String ?= null
+    private var password: String ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun observeEvent() {}
+
+    private fun register() {
+//        vm.register(RegisterRequest)
+    }
 }
